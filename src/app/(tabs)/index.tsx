47b0b53b540d8ce2cd4a4d8f1d92a4ac759700e1
Lucas da Silva <sklucassilva@gmail.com>
2024-07-30 18:19:48 -0300

    feat: Add search input and menu button to the header

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
 
 import { Text, View } from 'react-native'
+import { Input } from '@/components/input/input'
+import { MenuButton } from '@/components/menuButton/menu-button'
 
 export default function index() {
   return (
-    <View className="flex-1 bg-gray-900">
-      <Text>Hello World</Text>
+    <View className="flex-1 bg-gray-900 pt-14 p-4">
+      <Input>
+        <MenuButton />
+        <Input.Field placeholder="Pesquisar no e-mail" />
+      </Input>  
     </View>
   )
 }
