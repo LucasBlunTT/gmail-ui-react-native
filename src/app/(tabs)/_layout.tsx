69c35b1d9104d711c866662184c80ffb8bdd8424
Lucas da Silva <sklucassilva@gmail.com>
2024-09-11 14:55:12 -0300

    feat: Update tabs page to display emails with avatar and subject

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { colors } from '@/styles/colors';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -20,9 +21,30 @@ export default function TabLayout() {
         tabBarInactiveTintColor: colors.gray[400],
       }}
     >
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="chat" />
-      <Tabs.Screen name="meeting" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="email" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="chat-bubble" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="meeting"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="videocam" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
