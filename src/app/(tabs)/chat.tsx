5f34f868f38cd9e85ca1e302694317473ca23cca
Lucas da Silva <sklucassilva@gmail.com>
2024-09-11 14:34:43 -0300

    feat: Update tabs page to display emails with avatar and subject

---
+++ src/app/(tabs)/chat.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function Chat() {
+  return (
+    <View className="flex-1 bg-gray-900 pt-14 p-4 items-center justify-center">
+      <Text></Text>
+    </View>
+  );
+}
--- src/components/floatButton/floatButton.tsx
+++ src/components/floatButton/floatButton.tsx
@@ -5,10 +5,7 @@ import { colors } from '@/styles/colors';
 
 export default function FloatButton() {
   return (
-    <TouchableOpacity
-      className="px-8 py-4 bg-gray-600 rounded-full absolute bottom-4 right-4 gap-1 flex-row items-center"
-      activeOpacity={0.7}
-    >
+    <TouchableOpacity className="px-8 py-4 bg-gray-600 rounded-full absolute bottom-4 right-4 gap-1 flex-row items-center">
       <MaterialIcons name="edit" size={22} color={colors.orange[500]} />
       <Text className="text-orange-500 font-subtitle">Escrever</Text>
     </TouchableOpacity>
