b12d930f3561c2303b7a5c96c15279f85f5f8a04
Lucas da Silva <sklucassilva@gmail.com>
2024-09-11 14:51:07 -0300

    feat: Update tabs page to display emails with avatar and subject

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,11 +5,11 @@ import { Avatar } from '@/components/avatar/avatar';
 import { Email } from '@/components/email/email';
 import { EMAILS } from '@/utils/email';
 import FloatButton from '@/components/floatButton/floatButton';
-//import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function index() {
- // const navigation = useNavigation();
-
+  const navigation = useNavigation();
+  
   return (
     <View className="flex-1 bg-gray-900 pt-14 p-4">
       <Input>
